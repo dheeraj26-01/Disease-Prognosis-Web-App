3a98c17b7473e25e85164e931c942ffb0ab5b4af
Danish1875 <dhajiameen@gmail.com>
2023-05-08 16:39:08 +0530

    changes to navbar design

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,7 +8,7 @@ import Typewriter from "typewriter-effect";
 function HeroSection() {
   return (
     <div className='hero-container'>
-        <video src='videos/video-3.mp4' autoPlay loop muted />
+        <video src='videos/video-6.mp4' autoPlay loop muted />
         <Fade bottom>
         <h1>Get Checked Up</h1>
         </Fade>
